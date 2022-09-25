9aa1eeffe27e3805a2f6e04f100fe2bcb57e6e47
Dmytro Kolomiiets <dmytro_kolomiiets@epam.com>
2022-09-25 16:53:02 +0300

    Update tests.py file

--- tests.py
+++ tests.py
@@ -3,14 +3,14 @@ import os.path
 
 def test_model_file():
     flag = False
-    for root, dir, files in os.walk("."):
+    for root, dir, files in os.walk("/"):
         if "model.hdf5" in files:
             flag = True
     assert flag == True, "model.hdf5 file is not found"
 
 def test_labels_file():
     flag = False
-    for root, dir, files in os.walk("."):
+    for root, dir, files in os.walk("/"):
         if "labels.dat" in files:
             flag = True
     assert flag == True, "labels.dat file is not found"
